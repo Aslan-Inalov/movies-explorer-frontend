7bfee406dbe60e7c997482b791709f43121bf5e1
Aslan Inalov <aslaninalov@yandex.ru>
2023-07-09 18:42:21 +0300

    add form validation

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,79 +2,87 @@ import { useEffect, useState } from 'react';
 import './SearchForm.css';
 
 const SearchForm = ({ onFilter, searchQuery }) => {
-    const isChecked = JSON.parse(localStorage.getItem('filterCheckBox'));
-    const [isShortFilmChecked, setIsShortFilmChecked] = useState(isChecked);
-    const [searchText, setSearchText] = useState('');
+  const isChecked = JSON.parse(localStorage.getItem('filterCheckBox'));
+  const [isShortFilmChecked, setIsShortFilmChecked] = useState(isChecked);
+  const [searchText, setSearchText] = useState('');
+  const [error, setError] = useState('');
 
-    useEffect(() => {
-        if (searchQuery.searchText) {
-          setSearchText(searchQuery.searchText);
-        }
-      }, [searchQuery.searchText]);
 
-      const checkFilterBox = () => {
-        if (searchText !== '') {
-          setIsShortFilmChecked(!isShortFilmChecked);
-          localStorage.setItem('filterCheckBox', !isShortFilmChecked);
-    
-          onFilter({
-            searchText: searchText,
-            isShortFilmChecked: !isShortFilmChecked
-          });
-        } else {
-          setIsShortFilmChecked(!isShortFilmChecked);
-          localStorage.setItem('filterCheckBox', !isShortFilmChecked);
-    
-          onFilter({
-            searchText: searchQuery.searchText,
-            isShortFilmChecked: !isShortFilmChecked
-          });
-        }
-      };
+  useEffect(() => {
+    if (searchQuery.searchText) {
+      setSearchText(searchQuery.searchText);
+    }
+  }, [searchQuery.searchText]);
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        onFilter({ searchText, isShortFilmChecked });
-    };
+  const checkFilterBox = () => {
+    if (searchText !== '') {
+      setIsShortFilmChecked(!isShortFilmChecked);
+      localStorage.setItem('filterCheckBox', !isShortFilmChecked);
 
-    return (
-        <section className="search">
-            <form className='search-form' onSubmit={onSubmit}>
-                <div className='search-form__inner'>
-                    <input
-                        type='search'
-                        name='film-search'
-                        placeholder='Фильм'
-                        required
-                        minLength='1'
-                        maxLength='300'
-                        className='search-form__input'
-                        onChange={(e) => setSearchText(e.target.value)}
-                    />
-                    <button
-                        type='submit'
-                        className='search-form__button'
-                    >
-                        Найти
-                    </button>
-                </div>
-                <div className='search-form__toggle'>
-                    <label className='search-form__toggle-label' htmlFor='short-films'>
-                        <input
-                            className='search-form__toggle-checkbox-invisible'
-                            type='checkbox'
-                            name='short-films'
-                            id='short-films'
-                            onChange={checkFilterBox}
-                            checked={isShortFilmChecked || ''}
-                        />
-                        <span className={`search-form__toggle-checkbox-visible ${isShortFilmChecked && 'search-form__toggle-checkbox-visible_checked'}`} />
-                        Короткометражки
-                    </label>
-                </div>
-            </form>
-        </section>
-    );
+      onFilter({
+        searchText: searchText,
+        isShortFilmChecked: !isShortFilmChecked
+      });
+    } else {
+      setIsShortFilmChecked(!isShortFilmChecked);
+      localStorage.setItem('filterCheckBox', !isShortFilmChecked);
+
+      onFilter({
+        searchText: searchQuery.searchText,
+        isShortFilmChecked: !isShortFilmChecked
+      });
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (searchText.trim() === '') {
+      setError('Нужно ввести ключевое слово');
+    } else {
+      setError('');
+      onFilter({ searchText, isShortFilmChecked });
+    }
+  };
+
+  return (
+    <section className="search">
+      <form className='search-form' onSubmit={onSubmit} noValidate>
+        <div className='search-form__inner'>
+          <input
+            type='search'
+            name='film-search'
+            placeholder='Фильм'
+            required
+            minLength='1'
+            maxLength='300'
+            className='search-form__input'
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            type='submit'
+            className='search-form__button'
+          >
+            Найти
+          </button>
+        </div>
+        {error && <p className="search-form__error">{error}</p>}
+        <div className='search-form__toggle'>
+          <label className='search-form__toggle-label' htmlFor='short-films'>
+            <input
+              className='search-form__toggle-checkbox-invisible'
+              type='checkbox'
+              name='short-films'
+              id='short-films'
+              onChange={checkFilterBox}
+              checked={isShortFilmChecked || ''}
+            />
+            <span className={`search-form__toggle-checkbox-visible ${isShortFilmChecked && 'search-form__toggle-checkbox-visible_checked'}`} />
+            Короткометражки
+          </label>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
