720561637070285ee909de2655233de43edff59a
Aslan Inalov <aslaninalov@yandex.ru>
2023-07-10 16:28:15 +0300

    fix MoviesApi

--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -2,6 +2,7 @@ class MoviesApi {
   constructor({ url, headers }) {
     this._url = url;
     this._headers = headers;
+    this.moviesData = null;
   }
 
   _checkResponse(res) {
@@ -12,10 +13,15 @@ class MoviesApi {
   }
 
   getMovies() {
+    if (this.moviesData) { // Если данные фильмов уже загружены, возвращаем их
+      return Promise.resolve(this.moviesData);
+    }
     return fetch(`${this._url}`, { headers: this._headers })
-      .then((res) =>
-        this._checkResponse(res)
-      );
+      .then((res) => this._checkResponse(res))
+      .then((data) => {
+        this.moviesData = data; // Сохраняем данные фильмов
+        return data;
+      });
   }
 }
 
